40e2bbcea06c2c2594c32b6bdc2ee9c1b17c0960
Tiaog Wat <Tiaog Wat>
2018-02-10 19:02:16 +0000

    snd commit

---
+++ common/src/main/java/enums/ImgurRequestServiceUtils.java
@@ -0,0 +1,8 @@
+package enums;
+
+public class ImgurRequestServiceUtils {
+	
+	public static final String IMGUR_AUTH_HEADER = "Authorization";
+	public static final String IMGUR_REQUEST_URL = "";
+
+}
