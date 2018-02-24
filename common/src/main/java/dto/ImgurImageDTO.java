6f208445a6525b79874ec23c137d703338d41813
Tiaog <Tiaog@TiagoWat>
2018-02-24 16:25:40 +0000

    Merge

---
+++ common/src/main/java/dto/ImgurImageDTO.java
@@ -0,0 +1,169 @@
+package dto;
+
+public class ImgurImageDTO {
+	
+	private String animated;
+	private String bandwidth;
+	private String datetime;
+	private String deletehash;
+	private String description;
+	private String favorite;
+	private String gifv;
+	private String height;
+	private String id;
+	private String in_gallery;
+	private String link;
+	private String looping;
+	private String mp4;
+	private String mp4_size;
+	private String name;
+	private String nsfw;
+	private String section;
+	private String size;
+	private String title;
+	private String type;
+	private String views;
+	private String vote;
+	private String width;
+	public String getAnimated() {
+		return animated;
+	}
+	public void setAnimated(String animated) {
+		this.animated = animated;
+	}
+	public String getBandwidth() {
+		return bandwidth;
+	}
+	public void setBandwidth(String bandwidth) {
+		this.bandwidth = bandwidth;
+	}
+	public String getDatetime() {
+		return datetime;
+	}
+	public void setDatetime(String datetime) {
+		this.datetime = datetime;
+	}
+	public String getDeletehash() {
+		return deletehash;
+	}
+	public void setDeletehash(String deletehash) {
+		this.deletehash = deletehash;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getFavorite() {
+		return favorite;
+	}
+	public void setFavorite(String favorite) {
+		this.favorite = favorite;
+	}
+	public String getGifv() {
+		return gifv;
+	}
+	public void setGifv(String gifv) {
+		this.gifv = gifv;
+	}
+	public String getHeight() {
+		return height;
+	}
+	public void setHeight(String height) {
+		this.height = height;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getIn_gallery() {
+		return in_gallery;
+	}
+	public void setIn_gallery(String in_gallery) {
+		this.in_gallery = in_gallery;
+	}
+	public String getLink() {
+		return link;
+	}
+	public void setLink(String link) {
+		this.link = link;
+	}
+	public String getLooping() {
+		return looping;
+	}
+	public void setLooping(String looping) {
+		this.looping = looping;
+	}
+	public String getMp4() {
+		return mp4;
+	}
+	public void setMp4(String mp4) {
+		this.mp4 = mp4;
+	}
+	public String getMp4_size() {
+		return mp4_size;
+	}
+	public void setMp4_size(String mp4_size) {
+		this.mp4_size = mp4_size;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNsfw() {
+		return nsfw;
+	}
+	public void setNsfw(String nsfw) {
+		this.nsfw = nsfw;
+	}
+	public String getSection() {
+		return section;
+	}
+	public void setSection(String section) {
+		this.section = section;
+	}
+	public String getSize() {
+		return size;
+	}
+	public void setSize(String size) {
+		this.size = size;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getViews() {
+		return views;
+	}
+	public void setViews(String views) {
+		this.views = views;
+	}
+	public String getVote() {
+		return vote;
+	}
+	public void setVote(String vote) {
+		this.vote = vote;
+	}
+	public String getWidth() {
+		return width;
+	}
+	public void setWidth(String width) {
+		this.width = width;
+	}
+	
+	
+
+}
--- common/src/main/java/enums/ImgurRequestServiceUtils.java
+++ common/src/main/java/enums/ImgurRequestServiceUtils.java
@@ -2,7 +2,12 @@ package enums;
 
 public class ImgurRequestServiceUtils {
 	
-	public static final String IMGUR_AUTH_HEADER = "Authorization";
+	public static final String IMGUR_AUTH_HEADER_AUTH = "Authorization";
+	public static final String IMGUR_AUTH_HEADER_AUTH_CLIENT_ID = "Client-ID ";
+	public static final String IMGUR_AUTH_HEADER_AUTH_BEARER = "Bearer ";
 	public static final String IMGUR_REQUEST_URL = "";
-
+	public static final String IMGUR_REQUEST_URL_GET_IMAGE = "https://api.imgur.com/3/image/";
+	public static final String IMGUR_REQUEST_URL_GET_ALBUM = "https://api.imgur.com/3/album/{albumId}/images";
+	public static final String IMGUR_REQUEST_URL_GET_IMAGE_ACCOUNT = "https://api.imgur.com/3/account/{accountId}/images";
+	public static final String IMGUR_REQUEST_URL_GALLERY_TOP = "https://api.imgur.com/3/gallery/top";
 }
